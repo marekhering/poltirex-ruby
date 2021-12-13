f5e384a110df82150d139f8f3717e6bf32f73fb5
Marek <marek.hering99@gmail.com>
2021-12-13 20:37:19 +0100

    add automatic Route creation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,8 @@
 class Order < ApplicationRecord
-  has_one :order
+  has_one :route
+  after_create :create_belong_route
+
+  def create_belong_route
+    self.create_route(start_datetime: (self.delivery_time.to_time - 5.hours).to_datetime, end_datetime: self.delivery_time)
+  end
 end
