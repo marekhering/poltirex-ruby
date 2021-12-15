c62f4e0804b56f721c075f9cacb88bb86ce763b8
Marek <marek.hering99@gmail.com>
2021-12-15 18:50:17 +0100

    add init_app function

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,21 @@ class HomeController < ApplicationController
       redirect_to "/stretches"
     end
   end
+
+  def init_app
+    Stretch.delete_all
+    Truck.delete_all
+    Route.delete_all
+    Order.delete_all
+    User.delete_all
+
+    driver1 = Driver.create(email:"mirekg@gmail.com", login:"miroslawg", name:"Miroslaw", 
+                              surname:"Gorski", password:"gora", password_confirmation:"gora")
+    driver2 = Driver.create(email:"krzysiekw@gmail.com", login:"krzyszw", name:"Krzysztof", 
+                              surname:"Wazynski", password:"waga", password_confirmation:"waga")
+    Truck.create(height:2.1, capacity:50, position_lat: 54, position_lon:18, user_id:driver1.id)
+    Truck.create(height:2.3, capacity:60, position_lat: 53, position_lon:19, user_id:driver2.id)
+
+    redirect_to "/"
+  end
 end
